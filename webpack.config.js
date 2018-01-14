ff0eed7fd38afc120dd2649b0dd5fd828a19094a
bojidaryovchev <blizzardcon10@gmail.com>
2018-01-14 23:43:24 +0200

    Added webpack as module bundler, typescript support with tslint, nodemon and concurrently. Also, some @types for node since we're using typescript

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {};
+
+fs.readdirSync('node_modules')
+  .filter(function(moduleName) {
+    return moduleName !== '.bin';
+  })
+  .forEach(function(moduleName) {
+    nodeModules[moduleName] = 'commonjs ' + moduleName;
+  });
+
+module.exports = {
+    entry: path.resolve(__dirname, "./src/index.ts"),
+    output: {
+        path: path.resolve(__dirname, "./dist"), 
+        filename: "server.bundle.js"
+    },
+    target: "node",
+    externals: nodeModules,
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["ts-loader"],
+                exclude: /node_modules/
+            },
+            {
+                enforce: "pre",
+                test: /\.tsx?$/,
+                use: "tslint-loader",
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ ".ts", ".tsx" ]
+    }
+}
