852a5bc27d605e4f3d56eb38d774780e80c9fd0e
bojidaryovchev <blizzardcon10@gmail.com>
2018-01-23 18:57:02 +0200

    a slight rename for better readability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { IngredientComponent } from './shared/components/ingredient/ingredient.component';
 import { DropdownDirective } from './shared/directives/dropdown.directive';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { ServicesModule } from './services/services.module';
 import { GuardsModule } from './guards/guards.module';
 
