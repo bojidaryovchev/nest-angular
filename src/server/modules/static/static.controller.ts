ec6b4e710c6106201096580881df89a511d7a36d
bojidaryovchev <blizzardcon10@gmail.com>
2018-02-08 03:45:16 +0200

    removed redundant code from the static.controller

--- src/server/modules/static/static.controller.ts
+++ src/server/modules/static/static.controller.ts
@@ -1,19 +1,12 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { FOLDER_DIST_BROWSER } from '../../../shared/constants';
 import { join } from 'path';
-import { createReadStream } from 'fs';
 import * as e from 'express';
 
 @Controller()
 export class StaticController {
   @Get('*')
   all(@Res() res: e.Response, @Req() req: e.Request) {
-    const index: string = join(FOLDER_DIST_BROWSER, 'index.html');
-
-    if (process.env.NODE_ENV === 'production') {
-      res.render(index, { req });
-    } else {
-      createReadStream(index).pipe(res);
-    }
+      res.render(join(FOLDER_DIST_BROWSER, 'index.html'), { req });
   }
 }
