e47366ee4718c37090cee2e8e0b0aae30020c05e
bojidaryovchev <blizzardcon10@gmail.com>
2018-02-09 20:32:54 +0200

    Added CORS and Helmet middlewares

--- src/server/config/middlewares.ts
+++ src/server/config/middlewares.ts
@@ -1,12 +1,16 @@
 import { json, urlencoded } from 'body-parser';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 import { Application } from 'express';
 import * as session from 'express-session';
+import * as helmet from 'helmet';
 import { initialize, session as passportSession } from 'passport';
 import { IEnvironmentConfig } from './config';
 
 module.exports = (config: IEnvironmentConfig, app: Application) => {
   app.use(cookieParser());
+  app.use(cors());
+  app.use(helmet());
   app.use(json());
   app.use(urlencoded({ extended: true }));
   app.use(session({
