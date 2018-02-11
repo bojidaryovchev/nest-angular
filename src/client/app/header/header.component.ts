c576d5fac58cf98c7e5397009c33802a85c4c161
bojidaryovchev <blizzardcon10@gmail.com>
2018-02-11 02:47:48 +0200

    used some angular material for the navigation

--- src/client/app/header/header.component.ts
+++ src/client/app/header/header.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ISection {
+  title: string;
+  link: string;
+}
+
+interface INav {
+  sections: ISection[];
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  nav: INav = {
+    sections: [
+      {title: 'Recipes', link: 'recipes'},
+      {title: 'Shopping List', link: 'shopping-list'}
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
