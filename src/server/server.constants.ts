1cf838b174d150358337a4d49733168da9bab79b
bojidaryovchev <blizzardcon10@gmail.com>
2018-02-19 23:16:44 +0200

    addition to e1528f8fac398801705864d48b99e51de8af463d

--- src/server/server.constants.ts
+++ src/server/server.constants.ts
@@ -10,5 +10,6 @@ export const USER_MODEL_TOKEN: string = 'UserModelToken';
 export const MESSAGES = {
   UNAUTHORIZED_EMAIL_IN_USE: 'The email already exists',
   UNAUTHORIZED_INVALID_PASSWORD: 'Invalid password',
-  UNAUTHORIZED_INVALID_EMAIL: 'The email does not exist'
+  UNAUTHORIZED_INVALID_EMAIL: 'The email does not exist',
+  UNAUTHORIZED_UNRECOGNIZED_BEARER: 'Unrecognized bearer of the token'
 };
